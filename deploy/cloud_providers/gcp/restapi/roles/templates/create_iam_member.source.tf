7e66b223722a99dc54c12873d3bf62aa8929e58c
Julien4218 <jerard@newrelic.com>
2020-10-20 13:41:31 -0700

    Complete setup of GCP integration.

--- deploy/cloud_providers/gcp/restapi/roles/templates/create_iam_member.source.tf
+++ deploy/cloud_providers/gcp/restapi/roles/templates/create_iam_member.source.tf
@@ -1,12 +1,11 @@
 provider "google" {
   credentials = file("{{gcp_service_account_file}}")
-  project     = "{{gcp_project_id}}"
+  project     = "{{gcp_project}}"
   region      = "{{gcp_region}}"
 }
 
 resource "google_project_iam_member" "project" {
-  project     = "{{gcp_project_id}}"
+  project     = "{{gcp_project}}"
   role        = "roles/viewer"
   member      = "serviceAccount:{{authLabel}}"
-  description = "{{gcp_integration_name}}"
 }
